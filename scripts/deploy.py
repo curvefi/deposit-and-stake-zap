51ca15833bbc381262883e6469ff747285938d02
Michael Egorov <michwill@gmail.com>
2022-01-12 20:23:50 +0100

    Deployment script modified

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+from brownie import network
 from brownie import deposit_and_stake_zap, accounts
 
 
 def main():
-    return deposit_and_stake_zap.deploy({'from': accounts[0]})
+    txparams = {'from': accounts[0]}
+    if network.show_active() == 'mainnet':
+        accounts.load('babe')
+        txparams.update({'from': accounts[0], 'priority_fee': '2 gwei'})
+    return deposit_and_stake_zap.deploy(txparams)
