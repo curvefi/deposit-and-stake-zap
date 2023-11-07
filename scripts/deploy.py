6cbd496e0b9c363a77d0b736c2df586b6129fe3d
macket <macket24@proton.me>
2023-11-07 18:50:55 +0400

    ci: use curve-deployer for deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,9 +7,9 @@ from brownie import deposit_and_stake_zap, accounts
 def main():
     txparams = {}
     if network.show_active() == 'mainnet':
-        accounts.load('curve-tester')
+        accounts.load('curve-deployer')
         txparams.update({'priority_fee': '2 gwei'})
     elif not network.show_active().endswith("-fork"):
-        accounts.load('curve-tester')
+        accounts.load('curve-deployer')
     txparams.update({'from': accounts[0]})
     return deposit_and_stake_zap.deploy(txparams)
